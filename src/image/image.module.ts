e013bce43a8dc5f02ecfab3bf7d1d48a545402e9
bvpav <78971592+bvpav@users.noreply.github.com>
2024-01-13 18:16:03 +0200

    Added image entity

---
+++ src/image/image.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Image } from './image.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Image])],
+})
+export class ImageModule {
+}
