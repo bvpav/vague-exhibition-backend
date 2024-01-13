7b274399ad5bed78e483e0b386fdf3ada3865fea
bvpav <78971592+bvpav@users.noreply.github.com>
2024-01-13 18:19:18 +0200

    Brought back accidentally deleted module

---
+++ src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TFConfigModule } from '../tfconfig/tfconfig.module';
+import { TFConfigService } from '../tfconfig/tfconfig.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [TFConfigModule],
+      inject: [TFConfigService],
+      useFactory(tfConfigService: TFConfigService) {
+        return {
+          type: 'postgres',
+          url: tfConfigService.databaseUrl,
+          entities: [],
+          synchronize: tfConfigService.environment === 'development',
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {
+}
