696f1c43b3e60982e5b3624563a2028b3424817e
bvpav <78971592+bvpav@users.noreply.github.com>
2024-01-21 17:31:30 +0200

    Removed dummy code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CategoryService } from './project/category/category.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning();
-  const categoryService = app.get(CategoryService);
-  const categories = await categoryService.findAll();
-  console.log(categories);
   await app.listen(3000);
 }
 
