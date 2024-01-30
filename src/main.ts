516831c29cc34db4f14faf6165cb672f25a1e718
bvpav <78971592+bvpav@users.noreply.github.com>
2024-01-30 15:38:56 +0200

    Added Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning();
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Example')
+    .setDescription('An example NestJS application')
+    .setVersion('2.0')
+    .addTag('projects')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 
